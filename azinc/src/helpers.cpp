819189f388d437ae488d9e2e890dbea1b1fa2b1d
yydev-official <sihamelmachhouri@gmail.com>
2026-06-23 17:48:02 +0200

    Move helpers to azin::filesystem namespace
    Moved three helper functions to azin::filesystem.

--- azinc/src/helpers.cpp
+++ azinc/src/helpers.cpp
@@ -15,7 +15,7 @@
 namespace fs = std::filesystem;
 
 
-int azin::helpers::checkFileExists(const fs::path& source_path) {
+int azin::filesystem::checkFileExists(const fs::path& source_path) {
     if (!fs::exists(source_path)) {
         std::cerr << azin::ux::color::red << "Source file does not exist: " << source_path << azin::ux::color::reset << "\n";
         return 1;
@@ -23,7 +23,7 @@ int azin::helpers::checkFileExists(const fs::path& source_path) {
     return 0;
 }
 
-int azin::helpers::checkExtension(const fs::path& source_path) {
+int azin::filesystem::checkExtension(const fs::path& source_path) {
     if (source_path.extension() != ".az") {
         std::cerr << azin::ux::color::red << "Invalid source file extension!\nFile is not an azin source file " << source_path.extension() << azin::ux::color::reset << "\n";
         return 1;
@@ -31,7 +31,7 @@ int azin::helpers::checkExtension(const fs::path& source_path) {
     return 0;
 }
 
-std::ifstream azin::helpers::openSourceFile(const fs::path& source_path) {
+std::ifstream azin::filesystem::openSourceFile(const fs::path& source_path) {
     std::ifstream source_file(source_path);
     if (!source_file.is_open()) {
         std::cerr << azin::ux::color::red << "Failed to open source file: " << source_path << azin::ux::color::reset << "\n";
