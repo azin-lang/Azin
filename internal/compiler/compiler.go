65392361f2b6e685fe87ccd2497d1bed24a37545
Alecu Ștefan-Iulian <165364995+pascalecu@users.noreply.github.com>
2026-07-11 12:09:24 +0300

    Take source.File directly in compiler.Compile and shorten impl

--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -1,30 +1,14 @@
 package compiler
 
 import (
-	"fmt"
-
 	"github.com/azin-lang/Azin/internal/diagnostics"
 	"github.com/azin-lang/Azin/internal/lexer"
-	"github.com/azin-lang/Azin/internal/token"
+	"github.com/azin-lang/Azin/internal/source"
 )
 
-func Compile(source []byte, showTokens bool) error {
-	diag := diagnostics.New()
-	tokens := lex(source, diag)
-
-	if showTokens {
-		printTokens(tokens, source)
-	}
+func Compile(file *source.File) error {
+	diag := diagnostics.New(file)
+	_ = lexer.New(file, diag).Tokenize()
 
 	return diag.Err()
-}
-
-func lex(source []byte, diag *diagnostics.Engine) []token.Token {
-	return lexer.New(source, diag).Tokenize()
-}
-
-func printTokens(tokens []token.Token, source []byte) {
-	for _, tok := range tokens {
-		fmt.Println(tok.Format(source))
-	}
 }
