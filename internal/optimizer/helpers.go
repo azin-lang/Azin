626741a7f31e6cc8cbfec158aa214e38875598d4
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-07-18 02:13:55 +0300

    Let each AST expr node handle equality

--- internal/optimizer/helpers.go
+++ internal/optimizer/helpers.go
@@ -12,8 +12,6 @@ func isFalse(expr ast.Expr) bool {
 	return ok && !b.Value
 }
 
-// TODO: move this out of optimizer and implement an `Equal(Expr) bool` inside the Expr interface
-
 func exprEqual(left, right ast.Expr) bool {
 	// Fast path: identical pointers
 	if left == right {
@@ -24,51 +22,7 @@ func exprEqual(left, right ast.Expr) bool {
 		return false
 	}
 
-	switch l := left.(type) {
-	case *ast.IntegerLiteral:
-		r, ok := right.(*ast.IntegerLiteral)
-		return ok && l.Value == r.Value
-
-	case *ast.FloatLiteral:
-		r, ok := right.(*ast.FloatLiteral)
-		return ok && l.Value == r.Value
-
-	case *ast.BooleanLiteral:
-		r, ok := right.(*ast.BooleanLiteral)
-		return ok && l.Value == r.Value
-
-	case *ast.CharacterLiteral:
-		r, ok := right.(*ast.CharacterLiteral)
-		return ok && l.Value == r.Value
-
-	case *ast.Identifier:
-		r, ok := right.(*ast.Identifier)
-		return ok && l.Value == r.Value
-
-	case *ast.MemberExpr:
-		r, ok := right.(*ast.MemberExpr)
-		return ok && exprEqual(l.Object, r.Object) && exprEqual(l.Property, r.Property)
-
-	case *ast.BinaryExpr:
-		r, ok := right.(*ast.BinaryExpr)
-		return ok && l.Operator.Kind == r.Operator.Kind &&
-			exprEqual(l.Left, r.Left) && exprEqual(l.Right, r.Right)
-
-	case *ast.CallExpr:
-		r, ok := right.(*ast.CallExpr)
-		if !ok || len(l.Args) != len(r.Args) || !exprEqual(l.Callee, r.Callee) {
-			return false
-		}
-		for i := 0; i < len(l.Args); i++ {
-			if !exprEqual(l.Args[i], r.Args[i]) {
-				return false
-			}
-		}
-		return true
-
-	default:
-		return false
-	}
+	return left.Equals(right)
 }
 func isZero(expr ast.Expr) bool {
 	switch n := expr.(type) {
