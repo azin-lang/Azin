0f247d8fd159033d9915b17cf4a5b3ab6d710017
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-07-29 20:07:47 +0300

    Add relocation type

--- internal/obj/reloc.go
+++ internal/obj/reloc.go
@@ -1,1 +1,7 @@
 package obj
+
+type Relocation struct {
+	Offset uint32 // Where the placeholder is sitting in the .text machine code
+	Symbol string // What function it wants to call ("printf")
+	Type   uint8  // How to patch it (e.g. relative 32-bit jump)
+}
