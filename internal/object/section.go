80aa4a385945f35c4a8117335cfe0b62117f40ba
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-07-30 06:15:32 +0300

    Expand Section and Symbol types with more useful information

--- internal/object/section.go
+++ internal/object/section.go
@@ -1,7 +1,52 @@
 package object
 
+type SectionKind uint8
+
+const (
+	SectionUnknown SectionKind = iota
+
+	// Executable code
+	SectionText
+
+	// Read-only initialized data
+	SectionRodata
+
+	// Writable initialized data
+	SectionData
+
+	// Writable zero-initialized data
+	SectionBSS
+
+	// Thread-local initialized data
+	SectionTLSData
+
+	// Thread-local zero-initialized data
+	SectionTLSBSS
+
+	// Debug information
+	SectionDebug
+
+	// Exception handling / unwind tables
+	SectionException
+
+	// Symbol/string tables and other metadata
+	SectionMetadata
+)
+
+type SectionFlags uint32
+
+const (
+	SectionReadable SectionFlags = 1 << iota
+	SectionWritable
+	SectionExecutable
+	SectionAlloc
+)
+
 type Section struct {
 	Name        string
+	Kind        SectionKind
+	Flags       SectionFlags
+	Alignment   uint64
 	Data        []byte
-	Relocations []*Relocation
+	Relocations []Relocation
 }
--- internal/object/symbol.go
+++ internal/object/symbol.go
@@ -1,7 +1,30 @@
 package object
 
+type SymbolKind uint8
+
+const (
+	SymbolUnknown SymbolKind = iota
+
+	SymbolFunction
+	SymbolObject
+	SymbolSection
+	SymbolFile
+)
+
+type SymbolBinding uint8
+
+const (
+	BindingLocal SymbolBinding = iota
+	BindingGlobal
+	BindingWeak
+)
+
 type Symbol struct {
 	Name    string
+	Kind    SymbolKind
+	Binding SymbolBinding
+
 	Section *Section
 	Offset  uint64
+	Size    uint64
 }
