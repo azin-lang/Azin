ea420aec3790c89086fa2cb2acdc918b0c314a21
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-08-02 17:34:26 +0300

    Add keywords.go for exclusively dealing with keyword lookup

---
+++ internal/azin/syntax/keywords.go
@@ -0,0 +1,32 @@
+package syntax
+
+var keywords = map[string]SyntaxKind{
+	"defer":   KeywordDefer,
+	"do":      KeywordDo,
+	"else":    KeywordElse,
+	"end":     KeywordEnd,
+	"enum":    KeywordEnum,
+	"fn":      KeywordFn,
+	"for":     KeywordFor,
+	"if":      KeywordIf,
+	"import":  KeywordImport,
+	"importc": KeywordImportC,
+	"is":      KeywordIs,
+	"loop":    KeywordLoop,
+	"mut":     KeywordMut,
+	"return":  KeywordReturn,
+	"stop":    KeywordStop,
+	"struct":  KeywordStruct,
+	"then":    KeywordThen,
+	"type":    KeywordType,
+	"var":     KeywordVar,
+	"while":   KeywordWhile,
+}
+
+func LookupKeyword(text string) SyntaxKind {
+	if kind, ok := keywords[text]; ok {
+		return kind
+	}
+
+	return IdentifierToken
+}
