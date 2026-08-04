d35bfbfdd82701394dc092d7c82c806070225417
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-08-04 18:40:15 +0300

    Add bitwise flags inside green node base

--- internal/azin/syntax/green/base.go
+++ internal/azin/syntax/green/base.go
@@ -1,17 +1,30 @@
+// internal/azin/syntax/green/base.go
 package green
 
 import "github.com/azin-lang/Azin/internal/azin/syntax"
 
+type NodeFlags uint8
+
+const (
+	FlagNone                NodeFlags = 0
+	FlagContainsDiagnostics NodeFlags = 1 << iota
+	FlagIsMissing
+)
+
 type Base struct {
-	kind syntax.SyntaxKind
-
 	fullWidth uint32
+	kind      syntax.SyntaxKind
+	flags     NodeFlags
 }
 
-func (b Base) Kind() syntax.SyntaxKind {
-	return b.kind
+func (b Base) Kind() syntax.SyntaxKind { return b.kind }
+func (b Base) FullWidth() uint32       { return b.fullWidth }
+func (b Base) Flags() NodeFlags        { return b.flags }
+
+func (b Base) ContainsDiagnostics() bool {
+	return b.flags&FlagContainsDiagnostics != 0
 }
 
-func (b Base) FullWidth() uint32 {
-	return b.fullWidth
+func (b Base) IsMissing() bool {
+	return b.flags&FlagIsMissing != 0
 }
