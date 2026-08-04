52d3c55ab6b03c9d52328f7e889f7eabf137e6c6
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-08-04 15:01:19 +0300

    Add IsNil method to green node interface

--- internal/azin/syntax/green/interfaces.go
+++ internal/azin/syntax/green/interfaces.go
@@ -5,13 +5,11 @@ import "github.com/azin-lang/Azin/internal/azin/syntax"
 type Node interface {
 	Kind() syntax.SyntaxKind
 	FullWidth() uint32
-
 	GetLeadingTriviaWidth() uint32
 	GetTrailingTriviaWidth() uint32
-
 	SlotCount() int
 	Slot(int) Node
-
 	ContainsDiagnostics() bool
 	IsMissing() bool
+	IsNil() bool
 }
